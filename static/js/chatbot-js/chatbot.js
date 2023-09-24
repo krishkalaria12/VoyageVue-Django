57bab5c9dcfe0a32cb1f2b599967f1d467a83288
Krish Kalaria <96818172+CodeWithKrish12@users.noreply.github.com>
2023-09-24 22:33:33 +0530

    Update chatbot.js

--- static/js/chatbot-js/chatbot.js
+++ static/js/chatbot-js/chatbot.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
